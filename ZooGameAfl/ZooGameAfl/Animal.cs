ce34254211b3430649125cdb388e4947f791ca84
Miktastik <sike5@student.eadania.dk>
2024-11-19 17:47:50 +0100

    Start of Animal class
    Added strings and int's + start of "Mood"

---
+++ ZooGameAfl/ZooGameAfl/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooGameAfl
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int HungerLevel { get; set; } // 0 = fuld, 100 = meget sulten
+        public Mood CurrentMood { get; private set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+            HungerLevel = 50;
+            
+        }
+
+        
+    }
+}

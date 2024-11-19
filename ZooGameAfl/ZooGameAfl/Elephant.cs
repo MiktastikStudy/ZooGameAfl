7ebc824186b77d2c79c3af408fb812c7aed51813
Miktastik <sike5@student.eadania.dk>
2024-11-19 18:05:24 +0100

    Subclasser started
    Monkey og Elephant started

---
+++ ZooGameAfl/ZooGameAfl/Elephant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooGameAfl
+{
+    public class Elephant : Animal
+    {
+        public Elephant(string name) : base(name) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} trutter: PRRRRR!");
+        }
+    }
+}
---
+++ ZooGameAfl/ZooGameAfl/Monkey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ZooGameAfl
+{
+    public class Monkey : Animal
+    {
+        public Monkey(string name) : base(name) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} laver lyde: Ooh ooh aah aah!");
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + " Den elsker bananer!";
+        }
+    }
+}
